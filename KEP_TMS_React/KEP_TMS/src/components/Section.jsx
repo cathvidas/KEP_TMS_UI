d78a6179442203e2232956bf1d82e699943f2b2b
cathvidas <22104609@usc.edu.ph>
2024-09-03 23:15:22 +0800

    Removed deleted files again

--- KEP_TMS_React/KEP_TMS/src/components/Section.jsx
+++
@@ -1,8 +0,0 @@
-export const SectionTitle = ({ title, ViewAll }) => {
-  return (
-    <div className="d-flex justify-content-between">
-    <h6>{title}</h6>
-    {ViewAll && <p>{ViewAll}</p>}
-    </div>
-  );
-};
