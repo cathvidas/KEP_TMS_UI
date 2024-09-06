31a51158a9f8ac0c4b299ca50d032308ccdc1a8c
CORP\CVidas <Catherine.Vidas@knowles.com>
2024-09-06 17:29:18 +0800

    updated banner content

--- KEP_TMS/src/pages/NewRequest.jsx
+++ KEP_TMS/src/pages/NewRequest.jsx
@@ -1,8 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ActionButton } from "../components/Button";
 import { FormContainer } from "../components/Form";
 import TMS_Header from "../components/Header";
 import RenderLayout from "../components/Layout";
 import { SectionBanner, SectionTitle } from "../components/Section";
+import { faStickyNote } from "@fortawesome/free-solid-svg-icons";
 
 const NewRequest = () => {
   const action = () => (
@@ -22,11 +24,11 @@ const NewRequest = () => {
   const Content = () => {
     return (
       <>
-        <TMS_Header title="das" />
+        <TMS_Header title="Training Request" IconComponent={<FontAwesomeIcon icon={faStickyNote}/>}/>
         <SectionBanner
-          title="Add request"
-          subtitle={"I don "}
-          ActionComponents={action}
+          title="Training Requests || Add Request"
+          subtitle={"To add a new training request, fill out the form with the course name, desired date, and any special requirements. Click 'Submit' to finalize your request."}
+          // ActionComponents={action}
         />
         <SectionTitle title={"Recent Trainings"} ViewAll={"View All"} />
         <FormContainer />
