802829169f25af6af803bbdd375c20c465d3556b
CORP\CVidas <Catherine.Vidas@knowles.com>
2024-09-06 17:28:51 +0800

    added props accepting icon component

--- KEP_TMS/src/components/Header.jsx
+++ KEP_TMS/src/components/Header.jsx
@@ -2,26 +2,17 @@ import { Button, Navbar } from "react-bootstrap";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import proptype from 'prop-types'
 import {faPlus} from '@fortawesome/free-solid-svg-icons'
- const TMS_Header = ({ title }) => {
+ const TMS_Header = ({ title , IconComponent}) => {
   return (
     <Navbar className="navbar-expand-md d-flex">
-      <a className="navbar-brand d-flex align-items-center" href="#">
+      <a className="navbar-brand d-flex align-items-center fw-bold" href="#">
         <span
-          className="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center bs-icon"
+          className="d-flex justify-content-center align-items-center bs-icon"
           style={{ color: 'rgb(0, 167, 111)', background: 'rgba(13, 110, 253, 0)' }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-            className="bi bi-house-door"
-          >
-            <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"></path>
-          </svg>
+          {IconComponent && IconComponent}
+        <span className="ms-2 " style={{ color: "#00a76f" }}>{title}</span>
         </span>
-        <span style={{ color: "#00a76f" }}>{title}</span>
       </a>
       <button className="navbar-toggler" data-bs-toggle="collapse">
         <span className="visually-hidden">Toggle navigation</span>
@@ -85,6 +76,7 @@ import {faPlus} from '@fortawesome/free-solid-svg-icons'
   );
 }
 TMS_Header.propTypes ={
-  title: proptype.string.isRequired
+  title: proptype.string.isRequired,
+  IconComponent: proptype.object
 }
 export default TMS_Header
