456b431098b5ff3b01a7ce300ad256dc47f858d0
CurtDarryl <curtdada@gmail.com>
2024-09-06 10:50:24 +0800

    Added the system name to routes for easy navigation

--- KEP_TMS/src/App.jsx
+++ KEP_TMS/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
@@ -7,14 +6,13 @@ import { NotFoundPage } from "./pages/Error";
 import Login from "./pages/Login";
 import NewRequest from "./pages/NewRequest";
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/Request_View" element={<RequestView />} />
-        <Route path="/NewRequest" element={<NewRequest />} />
+        <Route path="/KEP_TMS" element={<Login />} />
+        <Route path="/KEP_TMS/Dashboard" element={<Dashboard />} />
+        <Route path="/KEP_TMS/Request_View" element={<RequestView />} />
+        <Route path="/KEP_TMS/NewRequest" element={<NewRequest />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
