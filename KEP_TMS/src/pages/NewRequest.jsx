c6b9d4131f5a294f29f0eff23e7de81470981b19
cathvidas <22104609@usc.edu.ph>
2024-09-08 23:56:38 +0800

    updated imports reference

--- KEP_TMS/src/pages/NewRequest.jsx
+++ KEP_TMS/src/pages/NewRequest.jsx
@@ -1,9 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ActionButton } from "../components/Button";
+import { ActionButton } from "../components/GeneralComponents/Button";
 import { FormContainer } from "../components/Form";
-import TMS_Header from "../components/Header";
-import RenderLayout from "../components/Layout";
-import { SectionBanner, SectionTitle } from "../components/Section";
+import TMS_Header from "../components/GeneralComponents/Header";
+import RenderLayout from "../components/GeneralComponents/Layout";
+import { SectionBanner } from "../components/GeneralComponents/Section";
 import { faStickyNote } from "@fortawesome/free-solid-svg-icons";
 
 const NewRequest = () => {
@@ -17,20 +17,24 @@ const NewRequest = () => {
       <ActionButton
         title="View All Request"
         actionLink="/KEP_TMS/Newrequest"
-        variant={{ brand: "secondary", size: "btn-sm" }}
+        variant={{ theme: "secondary", size: "btn-sm" }}
       />
     </>
   );
   const Content = () => {
     return (
       <>
-        <TMS_Header title="Training Request" IconComponent={<FontAwesomeIcon icon={faStickyNote}/>}/>
+        <TMS_Header
+          title="Training Request"
+          IconComponent={<FontAwesomeIcon icon={faStickyNote} />}
+        />
         <SectionBanner
           title="Training Requests || Add Request"
-          subtitle={"To add a new training request, fill out the form with the course name, desired date, and any special requirements. Click 'Submit' to finalize your request."}
-          // ActionComponents={action}
+          subtitle={
+            "To add a new training request, fill out the form with the course name, desired date, and any special requirements. Click 'Submit' to finalize your request."
+          }
+          //  ActionComponents={action}
         />
-        <SectionTitle title={"Recent Trainings"} ViewAll={"View All"} />
         <FormContainer />
       </>
     );
