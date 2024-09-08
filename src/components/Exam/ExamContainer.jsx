fd3d83b7db6be5131e7a0b88e22b4acd7bacc0bb
cathvidas <22104609@usc.edu.ph>
2024-09-09 05:34:26 +0800

    initialized exam components with randomize func

---
+++ src/components/Exam/ExamContainer.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { SampleExamQuestionaire } from "../../services/getApis";
+import { ExamItem } from "./ExamItem";
+
+const RandomizeExam = () => {
+  const exam = SampleExamQuestionaire().sort(() => Math.random() - 0.5);
+  return exam;
+};
+const ExamContainer = () => {
+   const [exam] = useState(()=>RandomizeExam());
+// const [exam] = useState(SampleExamQuestionaire);
+const [currentQuestion, setCurrentQuestion] = useState(exam[0]);
+console.log(currentQuestion)
+  const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  if (!exam || exam.length === 0 || index >= exam.length) {
+    return <div>Your score is  {score} / {exam.length}</div>;
+  }
+  const handleAnswer = (correct) => {
+    if (correct) {
+      setScore(score + 1);
+    }
+    if (index < exam.length) {
+        setTimeout(()=>{
+            setIndex(index + 1);
+            setCurrentQuestion(exam[index + 1]);
+        }, 5000)
+    } else {
+      alert(`Your score is ${score}/${exam.length}`);
+    }
+  };
+  return (
+    <div className="exam-container">
+      <h5>Questionnaire</h5>
+      {console.log(exam[0])}
+        <ExamItem
+          itemNo={`Question ${index + 1}`}
+          question={currentQuestion.question}
+          options={currentQuestion.options}
+          answer={currentQuestion.answer}
+          onAnswer={handleAnswer}
+        />
+    </div>
+  );
+};
+
+export default ExamContainer;
