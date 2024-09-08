2085c8c9ee9812a3521a64f405b0611a728bc0a5
cathvidas <22104609@usc.edu.ph>
2024-09-09 02:09:30 +0800

    modified

--- KEP_TMS/src/pages/Request_View.jsx
+++ KEP_TMS/src/pages/Request_View.jsx
@@ -1,50 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import TMS_Header from "../components/General/Header";
 import RenderLayout from "../components/General/Layout";
-import { RequestMenu } from "../components/Form/Menu";
+import { RequestMenu } from "../components/TrainingDetails/Menu";
+import { SectionHeading } from "../components/General/Section.jsx";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import TDOverview from "../components/TrainingDetails/TDetOverview.jsx";
+import TScheduleOverview from "../components/TrainingDetails/TSchedOverview.jsx";
+import { GetSchedule, SampleStringList } from "../services/getApis.jsx";
+import { TabHeader } from "../components/General/Tab.jsx";
 
 const Content = () => {
+  
   return (
     <>
       <TMS_Header title={"Training Detail"} />
       <div className="d-flex gap-3">
         <RequestMenu />
-        <div className="flex-grow-1">
-          <div
-            className="p-4 rounded"
-            style={{
-              background:
-                "linear-gradient(rgba(91,228,155,0.2) 0%, rgba(0,167,111,0.2) 98%)",
-            }}
-          >
-            <h3 className="fw-bold" style={{ color: "rgb(0,75,80)" }}>
-              Training Requests || Add Request
-            </h3>
-            <p className="m-0">
-              To add a new training request, fill out the form with the course
-              name, desired date, and any special requirements. Click
-              &apos;Submit to finalize your request.
-            </p>
-            <div className="d-flex mt-3">
-              <a
-                className="btn btn-primary btn-lg me-2"
-                role="button"
-                href="#"
-                style={{ background: "#00a76f", borderColor: "#00a76f" }}
-                data-bs-toggle="modal"
-                data-bs-target="#TRtype"
-              >
-                Request Training
-              </a>
-              <a
-                className="btn btn-lg"
-                role="button"
-                href="#"
-                style={{ background: "#f6fbf9", color: "#066337" }}
-              >
-                View Request
-              </a>
-            </div>
-          </div>
+        <div className="flex-fill">
+          <SectionHeading
+            title="Overview"
+            icon={<FontAwesomeIcon icon={faInfoCircle} />}
+          />
+          <p>Here you can see the details of your training.</p>
+          <TDOverview/>
+          <TScheduleOverview schedule={GetSchedule()}/>
+          <br />
+          <TabHeader tablist={SampleStringList()} activeItem={"Apple"}/>
         </div>
       </div>
     </>
