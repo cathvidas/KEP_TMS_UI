bdcb6818f7adbac8025b34ba51cd5b1c891ec097
cathvidas <22104609@usc.edu.ph>
2024-11-25 06:17:32 +0800

    added external facilitator list

---
+++ KEP_TMS/src/components/List/ExternalFacilitatorList.jsx
@@ -0,0 +1,59 @@
+import CommonTable from "../General/CommonTable";
+import { Button } from "primereact/button";
+import proptype from "prop-types"
+import externalFacilitatorHook from "../../hooks/externalFacilitatorHook";
+
+const ExternalFacilitatorList = ({
+  trainers,
+  removeTrainer,
+  showDeleteButton,
+}) => {
+  const mappedData = externalFacilitatorHook.useListExternalFacilitators(trainers)
+  const actionBodyTemplate = (data) => {
+    return (
+      <Button
+        type="button"
+        severity="danger"
+        icon="pi pi-trash"
+        text
+        // onClick={() => removeTrainer(data.employeeBadge)}
+      />
+    );
+  };
+  console.log(trainers, mappedData)
+  const columnItems = [
+    {
+      field: "",
+      header: "No",
+      body: (_, { rowIndex }) => <>{1 + rowIndex}</>,
+    },
+    {
+      field: "name",
+      header: "Name",
+    },
+    {
+      field: "position",
+      header: "Position",
+    },
+    {
+      field: "depatmentOrganization",
+      header: "Depatment Organization",
+    },
+    // ...(showDeleteButton && {
+    //   field: "",
+    //   header: "Action",
+    //   body: actionBodyTemplate,
+    // }),
+  ];
+  return (
+    <>
+      <CommonTable columnItems={columnItems} dataTable={mappedData} />
+    </>
+  );
+};
+ExternalFacilitatorList.propTypes = {
+  trainers: proptype.array,
+  removeTrainer: proptype.func,
+  showDeleteButton: proptype.bool,
+}
+export default ExternalFacilitatorList;
