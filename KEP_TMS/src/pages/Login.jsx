86d3e7407117086d3a3e679a968e60c7f6914067
CORP\CVidas <Catherine.Vidas@knowles.com>
2025-04-02 09:27:12 +0800

    disabled button upon processing request

--- KEP_TMS/src/pages/Login.jsx
+++ KEP_TMS/src/pages/Login.jsx
@@ -29,6 +29,7 @@ const Login = () => {
   // response variables
   const handleLogin = async (e) => {
     e.preventDefault();
+    if(loading) {return};
     if (validateLogin(badge, password)) {
       var data = {
         employeeId: e.target.badge.value?.trim(),
@@ -142,7 +143,7 @@ const Login = () => {
                     </Form.Group>}
                     <Form.Group className="mb-3">
                       <Button
-                        className="d-block w-100 p-3 rounded"
+                        className={`d-block w-100 p-3 rounded ${loading ? "opacity-50" : ""}`}
                         type="submit"
                         style={{
                           background: "#2eb396",
